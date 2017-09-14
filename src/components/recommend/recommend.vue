3567261f4a6cd8930d7e232649833cc2a3f26736
LIZEZHONG <2546406321@qq.com>
2017-09-14 22:26:20 +0800

    歌单列表滚动

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,33 +1,36 @@
 <template>
 	<div class="recommend">
-		<div class="recommend-content">
-			<div v-if="recommends.length" class="slider-wrapper">
-				<slider>
-					<div v-for="item in recommends">
-						<a :href="item.linkUrl">
-							<img :src="item.picUrl">
-						</a>
-					</div>
-				</slider>
-			</div>
-			<div class="recommend-list">
-				<h1 class="list-title">热门歌单推荐</h1>
-				<ul>
-					<li v-for="item in discList" class="item">
-						<div class="icon">
-							<img width="60" height="60" :src="item.imgurl">
+		<scroll class="recommend-content" :data="discList">
+			<div>
+				<div v-if="recommends.length" class="slider-wrapper">
+					<slider>
+						<div v-for="item in recommends">
+							<a :href="item.linkUrl">
+								<img :src="item.picUrl">
+							</a>
 						</div>
-						<div class="text">
-							<h2 class="name" v-html="item.creator.name"></h2>
-							<p class="desc" v-html="item.dissname"></p>
-						</div>
-					</li>
-				</ul>
+					</slider>
+				</div>
+				<div class="recommend-list">
+					<h1 class="list-title">热门歌单推荐</h1>
+					<ul>
+						<li v-for="item in discList" class="item">
+							<div class="icon">
+								<img width="60" height="60" :src="item.imgurl">
+							</div>
+							<div class="text">
+								<h2 class="name" v-html="item.creator.name"></h2>
+								<p class="desc" v-html="item.dissname"></p>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</div>
-		</div>
+		</scroll>
 	</div>
 </template>
 <script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
   import {getRecommend, getDiscList} from 'api/recommend'
   import Slider from 'base/slider/slider'
   import {ERR_OK} from 'api/config'
@@ -39,7 +42,8 @@
       }
     },
     components: {
-      Slider
+      Slider,
+      Scroll
     },
     created() {
       this._getRecommend()
