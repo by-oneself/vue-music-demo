c97a9b43b3935c1c6618165bd60daac0666a6735
LIZEZHONG <2546406321@qq.com>
2017-09-14 23:17:09 +0800

    lazyload

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
 /* eslint-disable no-new */
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 new Vue({
   el: '#app',
   router,
