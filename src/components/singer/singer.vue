bc66241380de1973161fda1e4080416586bb0cdf
LIZEZHONG <2546406321@qq.com>
2017-09-18 21:25:12 +0800

    抓取歌手列表数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -4,6 +4,33 @@
 	</div>
 </template>
 <script type="text/ecmascript-6">
-	
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this.singers)
+          }
+        })
+      }
+    }
+  }
 </script>
-<style lang="stylus" rel="stylesheet/stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
