19b7a3fe08025865ddc3a42fab302d0535d647b6
LIZEZHONG <2546406321@qq.com>
2017-09-18 22:37:26 +0800

    歌手数据处理以及inger类的封装

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,6 +6,10 @@
 <script type="text/ecmascript-6">
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+  console.log(Singer)
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
   export default {
     data() {
       return {
@@ -23,6 +27,48 @@
             console.log(this.singers)
           }
         })
+      },
+      _nomalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        // 得到有序列表 需要处理map
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
     }
   }
