d2a18a9b68b39405f1250d97adc0d15c343ebb68
LIZEZHONG <2546406321@qq.com>
2017-09-21 21:41:49 +0800

    vuex搭建

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -9,6 +9,7 @@
   import {ERR_OK} from 'api/config'
   import Singer from 'common/js/singer'
   import listView from 'base/listview/listview'
+
   const HOT_NAME = '热门'
   const HOT_SINGER_LEN = 10
   export default {
---
+++ src/store/mutations.js
@@ -0,0 +1,9 @@
+import * as types from './mutations-types'
+
+const matutaions = {
+  [types.SET_SINGER](state, singer) {
+    state.singer = singer
+  }
+}
+
+export default matutaions
