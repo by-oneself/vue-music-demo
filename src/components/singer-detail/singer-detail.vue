6a686072e9d42ad28cc21bd447e28a40b5531f42
LIZEZHONG <2546406321@qq.com>
2017-09-21 23:08:49 +0800

    歌手详情数据抓取

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -9,6 +9,9 @@
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+
   export default {
     computed: {
       ...mapGetters([
@@ -16,7 +19,20 @@
       ])
     },
     created() {
-      console.log(this.singer)
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)
+          }
+        })
+      }
     }
   }
 
