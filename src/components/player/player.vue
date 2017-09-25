d0437c4a5bade710a698b0dbec5290ecf87ee117
LIZEZHONG <2546406321@qq.com>
2017-09-26 00:14:52 +0800

    播放器歌曲播放功能实现

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -15,7 +15,7 @@
 				<div class="middle">
 					<div class="middle-l">
 						<div class="cd-wrapper" ref="cdWrapper">
-							<div class="cd">
+							<div class="cd" :class="cdCls">
 								<img class="image" :src="currentSong.image">
 							</div>
 						</div>
@@ -30,7 +30,7 @@
 							<i class="icon-prev"></i>
 						</div>
 						<div class="icon i-center">
-							<i class="icon-play"></i>
+							<i @click="togglePlaying" :class="playIcon"></i>
 						</div>
 						<div class="icon i-right">
 							<i class="icon-next"></i>
@@ -45,20 +45,21 @@
 		<transition name="mini">
 			<div class="mini-player" v-show="!fullScreen" @click="open">
 				<div class="icon">
-					<img width="40" height="40" :src="currentSong.image">
+					<img :class="cdCls" width="40" height="40" :src="currentSong.image">
 				</div>
 				<div class="text">
 					<h2 class="name" v-html="currentSong.name"></h2>
 					<p class="desc" v-html="currentSong.singer"></p>
 				</div>
 				<div class="control">
-					<i class="icon-play-mini"></i>
+					<i @click.stop="togglePlaying" :class="miniInco"></i>
 				</div>
 				<div class="control">
 					<i class="icon-playlist"></i>
 				</div>
 			</div>
 		</transition> 
+		<audio ref="audio" :src="currentSong.url"></audio>
 	</div>
 </template>
 
@@ -69,10 +70,20 @@
   const transform = prefixStyle('transform')
   export default {
     computed: {
+      cdCls() {
+        return this.playing ? 'play' : 'play pause'
+      },
+      playIcon() {
+        return this.playing ? 'icon-pause' : 'icon-play'
+      },
+      miniInco() {
+        return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+      },
       ...mapGetters([
         'fullScreen',
         'playlist',
-        'currentSong'
+        'currentSong',
+        'playing'
       ])
     },
     methods: {
@@ -122,6 +133,9 @@
         this.$refs.cdWrapper.style.transition = ''
         this.$refs.cdWrapper.style[transform] = ''
       },
+      togglePlaying() {
+        this.setPlayingState(!this.playing)
+      },
       _getPosAndScale() {
         const targetWidth = 40
         const paddingLeft = 40
@@ -138,8 +152,22 @@
         }
       },
       ...mapMutations({
-        setFullScreen: 'SET_PULL_SCREEN'
+        setFullScreen: 'SET_PULL_SCREEN',
+        setPlayingState: 'SET_PLAYING_STATE'
       })
+    },
+    watch: {
+      currentSong() {
+        this.$nextTick(() => {
+          this.$refs.audio.play()
+        })
+      },
+      playing(newPlaying) {
+        const audio = this.$refs.audio
+        this.$nextTick(() => {
+          newPlaying ? audio.play() : audio.pause()
+        })
+      }
     }
   }
 </script>
