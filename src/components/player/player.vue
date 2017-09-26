c53da1528903012913f9f8206edb963828e1b754
LIZEZHONG <2546406321@qq.com>
2017-09-26 21:22:52 +0800

    歌曲播放前进后退功能

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -26,13 +26,13 @@
 						<div class="icon i-left">
 							<i class="icon-sequence"></i>
 						</div>
-						<div class="icon i-left">
+						<div class="icon i-left" :class="disableCls">
 							<i @click="prev" class="icon-prev"></i>
 						</div>
-						<div class="icon i-center">
+						<div class="icon i-center" :class="disableCls">
 							<i @click="togglePlaying" :class="playIcon"></i>
 						</div>
-						<div class="icon i-right">
+						<div class="icon i-right" :class="disableCls">
 							<i @click="next" class="icon-next"></i>
 						</div>
 						<div class="icon i-right">
@@ -83,6 +83,9 @@
       },
       miniInco() {
         return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+      },
+      disableCls() {
+        return this.songReady ? '' : 'disable'
       },
       ...mapGetters([
         'fullScreen',
@@ -177,6 +180,7 @@
         this.songReady = true
       },
       error() {
+        this.songReady = true
       },
       _getPosAndScale() {
         const targetWidth = 40
