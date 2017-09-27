5c4af094b40fbc3b9068a79152006c4a13ec7ea6
LIZEZHONG <2546406321@qq.com>
2017-09-27 22:01:59 +0800

    播放器进度条点击播放功能

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="progress-bar" ref="progressBar">
+	<div class="progress-bar" ref="progressBar" @click="progressClick">
 		<div class="bar-inner">
 			<div class="progress" ref="progress">
 				<div class="progress-btn-wrapper" ref="progressBtn"
@@ -44,6 +44,10 @@
       },
       progressTouchEnd() {
         this.touch.initiated = false
+        this._triggerPercent()
+      },
+      progressClick(e) {
+        this._offset(e.offsetX)
         this._triggerPercent()
       },
       _triggerPercent() {
