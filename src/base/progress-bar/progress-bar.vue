3b17555a78567fd067bf46e3ef70b59008756b49
LIZEZHONG <2546406321@qq.com>
2017-09-30 00:22:31 +0800

    播放器随机播放全部功能

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -47,7 +47,11 @@
         this._triggerPercent()
       },
       progressClick(e) {
-        this._offset(e.offsetX)
+        const rect = this.$refs.progressBar.getBoundingClientRect()
+        const offsetWidth = e.pageX - rect.left
+        this._offset(offsetWidth)
+        // 点击progressBtn的时候e.offsetX获取不当
+        // this._offset(e.offsetX)
         this._triggerPercent()
       },
       _triggerPercent() {
