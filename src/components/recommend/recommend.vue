8ca3c1f9864be1ed84ffa1d6235ae5e1a459b105
LIZEZHONG <2546406321@qq.com>
2017-10-11 23:43:41 +0800

    播放器底部播放器适配与mixin的应用

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="recommend">
+	<div class="recommend" ref="recommend">
 		<scroll ref="scroll" class="recommend-content" :data="discList">
 			<div>
 				<div v-if="recommends.length" class="slider-wrapper">
@@ -38,7 +38,9 @@
   import {getRecommend, getDiscList} from 'api/recommend'
   import Slider from 'base/slider/slider'
   import {ERR_OK} from 'api/config'
+  import {playlistMixin} from 'common/js/mixin'
   export default {
+    mixins: [playlistMixin],
     data() {
       return {
         recommends: [],
@@ -55,6 +57,11 @@
       this._getDiscList()
     },
     methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.recommend.style.bottom = bottom
+        this.$refs.scroll.refresh()
+      },
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="singer">
-		<list-view :data="singers" @select="selectSinger"></list-view>
+	<div class="singer" ref="singer">
+		<list-view :data="singers" @select="selectSinger" ref="list"></list-view>
     <router-view></router-view>
 	</div>
 </template>
@@ -10,10 +10,12 @@
   import Singer from 'common/js/singer'
   import listView from 'base/listview/listview'
   import {mapMutations} from 'vuex'
+  import {playlistMixin} from 'common/js/mixin'
 
   const HOT_NAME = '热门'
   const HOT_SINGER_LEN = 10
   export default {
+    mixins: [playlistMixin],
     components: {
       listView
     },
@@ -26,6 +28,11 @@
       this._getSingerList()
     },
     methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.singer.style.bottom = bottom
+        this.$refs.list.refresh()
+      },
       selectSinger(singer) {
         this.$router.push({
           path: `/singer/${singer.id}`
