89e3edc2a67aed9b47fb54a789d2e38994199038
LIZEZHONG <2546406321@qq.com>
2017-10-19 00:15:01 +0800

    歌单详情页数据的处理和应用

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,15 +1,17 @@
 <template>
-	<transition name="slide">
-		<music-list :title="title" :bg-image="bgImage"></music-list>
-	</transition>
+  <transition name="slide">
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+  </transition>
 </template>
 
-<script type="text/javascript">
+<script type="text/ecmascript-6">
   import MusicList from 'components/music-list/music-list'
-  import {mapGetters} from 'vuex'
   import {getSongList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
-  export default{
+  import {mapGetters} from 'vuex'
+  import {createSong} from 'common/js/song'
+
+  export default {
     computed: {
       title() {
         return this.disc.dissname
@@ -21,16 +23,34 @@
         'disc'
       ])
     },
+    data() {
+      return {
+        songs: []
+      }
+    },
     created() {
       this._getSongList()
     },
     methods: {
       _getSongList() {
+        if (!this.disc.dissid) {
+          this.$router.push('/recommend')
+          return
+        }
         getSongList(this.disc.dissid).then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.cdlist[0].songlist)
+            this.songs = this._normalizeSongs(res.cdlist[0].songlist)
           }
         })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
       }
     },
     components: {
